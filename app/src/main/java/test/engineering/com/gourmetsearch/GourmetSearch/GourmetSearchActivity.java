9cd4e6acc0c3f0d72d5d207b535f558893495cc0
S N <seidi.nakamura@st-ventures.jp>
2019-04-20 16:17:43 +0900

    【飲食店検索画面】新規作成

--- app/src/main/java/test/engineering/com/gourmetsearch/GourmetSearch/GourmetSearchActivity.java
+++ app/src/main/java/test/engineering/com/gourmetsearch/GourmetSearch/GourmetSearchActivity.java
@@ -6,13 +6,21 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,28 +34,61 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import at.wirecube.additiveanimations.additive_animator.AdditiveAnimator;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import test.engineering.com.gourmetsearch.Model.API.APIInterface;
+import test.engineering.com.gourmetsearch.Model.API.APIService;
+import test.engineering.com.gourmetsearch.Model.Response.HotPepperObject;
+import test.engineering.com.gourmetsearch.Model.Response.Shop;
+import test.engineering.com.gourmetsearch.Model.Response.StoreResponse;
 import test.engineering.com.gourmetsearch.R;
 import test.engineering.com.gourmetsearch.Util.BitmapUtil;
 import test.engineering.com.gourmetsearch.Util.NetworkStateReceiver;
 
-public class GourmetSearchActivity extends FragmentActivity implements OnMapReadyCallback, NetworkStateReceiver.NetworkStateReceiverListener {
+public class GourmetSearchActivity extends FragmentActivity implements OnMapReadyCallback, NetworkStateReceiver.NetworkStateReceiverListener, GoogleMap.OnMarkerClickListener {
     public static final int LOCATION_REQUEST = 111;
 
     private NetworkStateReceiver networkStateReceiver;
     private GoogleMap mMap;
     private Marker myLocation;
+    private List<StoreResponse> storeList = new ArrayList<>();
+    private List<Marker> storeMarkerList = new ArrayList<>();
     private Circle circle;
 
-    // GPS用
     private LocationManager mLocationManager;
+
+    private ConstraintLayout genreSelectBar;
+    private TextView genreNameTextView;
+    private ConstraintLayout searchConstraintLayout;
+    private ImageView searchImageView;
+
+    private ConstraintLayout moveConstraintLayout;
+
+    private ConstraintLayout currentPositionConstraintLayout;
+
+    private ConstraintLayout storeDetailConstraintLayout;
+    private TextView alphabetTextView;
+    private TextView storeNameTextView;
+    private TextView openTextView;
+    private TextView minToGoTextView;
+
+    private Point point;
+    int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gourmet_search);
+
+        setupView();
 
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
@@ -59,6 +100,77 @@ public class GourmetSearchActivity extends FragmentActivity implements OnMapRead
             return;
         }
         setup();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Display display = getWindowManager().getDefaultDisplay();
+        point = new Point();
+        display.getSize(point);
+
+        setupGenreSelectBar();
+        setupStoreDetailConstraintLayout();
+        setupCurrentPositionConstraintLayout();
+    }
+
+    private void setupView() {
+        genreSelectBar = findViewById(R.id.genreSelectBar);
+        genreNameTextView = findViewById(R.id.genreNameTextView);
+        searchConstraintLayout = findViewById(R.id.searchConstraintLayout);
+        searchImageView = findViewById(R.id.searchImageView);
+
+
+        moveConstraintLayout = findViewById(R.id.moveConstraintLayout);
+
+        currentPositionConstraintLayout = findViewById(R.id.currentPositionConstraintLayout);
+
+        storeDetailConstraintLayout = findViewById(R.id.storeDetailConstraintLayout);
+        alphabetTextView = findViewById(R.id.alphabetTextView);
+        storeNameTextView = findViewById(R.id.storeNameTextView);
+        openTextView = findViewById(R.id.openTextView);
+        minToGoTextView = findViewById(R.id.minToGoTextView);
+    }
+
+    private void setupGenreSelectBar() {
+        genreSelectBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: ジャンル選択画面へ遷移
+            }
+        });
+
+        searchConstraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getStoreInfo();
+            }
+        });
+    }
+
+    private void setupCurrentPositionConstraintLayout() {
+        currentPositionConstraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (myLocation == null) {
+                    return;
+                }
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(myLocation.getPosition());
+                mMap.moveCamera(cameraUpdate);
+            }
+        });
+    }
+
+
+    private void setupStoreDetailConstraintLayout() {
+        storeDetailConstraintLayout.setVisibility(View.GONE);
+        storeNameTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: 飲食店詳細画面へ遷移
+            }
+        });
     }
 
     private void setup() {
@@ -113,6 +225,8 @@ public class GourmetSearchActivity extends FragmentActivity implements OnMapRead
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        mMap.setOnMarkerClickListener(this);
+
         List<String> providers = mLocationManager.getProviders(true);
         Location bestLocation = null;
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
@@ -165,6 +279,58 @@ public class GourmetSearchActivity extends FragmentActivity implements OnMapRead
                 .fillColor(getColor(R.color.colorPrimaryTransparent)));
     }
 
+    private void getStoreInfo() {
+        if (myLocation == null) {
+            return;
+        }
+        LatLng position = myLocation.getPosition();
+        APIInterface apiInterface = APIService.createService(APIInterface.class);
+        Call<HotPepperObject> call = apiInterface.getOptionsHotPepperObjectNew(
+                getString(R.string.hotpepperApikey),
+                "",
+                "json",
+                position.latitude,
+                position.longitude,
+                26
+        );
+        call.enqueue(new Callback<HotPepperObject>() {
+            @Override
+            public void onResponse(Call<HotPepperObject> call, Response<HotPepperObject> response) {
+                if (response.isSuccessful()) {
+                    storeList = response.body().getResults().getShop();
+                    receivedStoreDataList();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HotPepperObject> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void receivedStoreDataList() {
+        if (storeList.isEmpty()) {
+            Toast.makeText(getApplicationContext(), R.string.emptyStoreList, Toast.LENGTH_LONG);
+            return;
+        }
+        resetStoreMarkerList();
+        for (int position = 0; position < storeList.size(); position++) {
+            StoreResponse store = storeList.get(position);
+            LatLng location = new LatLng(store.getLat(), store.getLng());
+            storeMarkerList.add(mMap.addMarker(new MarkerOptions().position(location).title(String.format("%c", 'A' + position)).visible(true)));
+        }
+    }
+
+    private void resetStoreMarkerList() {
+        if (storeMarkerList.isEmpty()) {
+            return;
+        }
+        for (Marker storeMarker : storeMarkerList) {
+            storeMarker.remove();
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == LOCATION_REQUEST) {
@@ -175,10 +341,33 @@ public class GourmetSearchActivity extends FragmentActivity implements OnMapRead
     @Override
     public void networkAvailable() {
         Log.d("GourmetSearchActivity", "Connected");
+        searchConstraintLayout.setEnabled(true);
     }
 
     @Override
     public void networkUnavailable() {
         Log.d("GourmetSearchActivity", "Disonnected");
+        searchConstraintLayout.setEnabled(false);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        position = marker.getTitle().toCharArray()[0] - 'A';
+
+        StoreResponse store = storeList.get(position);
+
+        storeNameTextView.setText(store.getName());
+        alphabetTextView.setText(marker.getTitle());
+        if(store.getOpen() == null || store.getOpen().isEmpty()) {
+            openTextView.setText(getResources().getString(R.string.unknownOpen));
+        } else {
+            openTextView.setText(store.getOpen());
+        }
+        float[] results = new float[3];
+        Location.distanceBetween(myLocation.getPosition().latitude, myLocation.getPosition().longitude, marker.getPosition().latitude, marker.getPosition().longitude, results);
+        float distance = results[0];
+        minToGoTextView.setText(getResources().getString(R.string.to_minutes, Math.round(distance / 80.0f)));
+        storeDetailConstraintLayout.setVisibility(View.VISIBLE);
+        return true;
     }
 }
--- app/src/main/java/test/engineering/com/gourmetsearch/Launch/MainActivity.java
+++ app/src/main/java/test/engineering/com/gourmetsearch/Launch/MainActivity.java
@@ -4,17 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import test.engineering.com.gourmetsearch.GourmetSearch.GourmetSearchActivity;
 import test.engineering.com.gourmetsearch.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String REALM_DATABASE = "gourmet_search.realm";
+    private static final int REALM_DATABASE_VERSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initRealm();
+
         Intent gourmetSearchIntent = new Intent(getApplicationContext(), GourmetSearchActivity.class);
         startActivity(gourmetSearchIntent);
+    }
+    private void initRealm() {
+        Realm.init(this);
+        RealmConfiguration configuration  = new RealmConfiguration.Builder()
+                .name(REALM_DATABASE)
+                .schemaVersion(REALM_DATABASE_VERSION)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        //Now set this config as the default config for your app
+        //This way you can call Realm.getDefaultInstance elsewhere
+        Realm.setDefaultConfiguration(configuration);
     }
 }
