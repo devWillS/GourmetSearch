7ed9c15524ca5a74c481ca3c20dbcab795066d55
S N <seidi.nakamura@st-ventures.jp>
2019-04-20 12:57:25 +0900

    マップにピンとサークルを追加

--- app/src/main/java/test/engineering/com/gourmetsearch/GourmetSearch/GourmetSearchActivity.java
+++ app/src/main/java/test/engineering/com/gourmetsearch/GourmetSearch/GourmetSearchActivity.java
@@ -1,35 +1,106 @@
 package test.engineering.com.gourmetsearch.GourmetSearch;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+import java.util.Objects;
+
 import test.engineering.com.gourmetsearch.R;
+import test.engineering.com.gourmetsearch.Util.BitmapUtil;
 import test.engineering.com.gourmetsearch.Util.NetworkStateReceiver;
 
 public class GourmetSearchActivity extends FragmentActivity implements OnMapReadyCallback, NetworkStateReceiver.NetworkStateReceiverListener {
+    public static final int LOCATION_REQUEST = 111;
+
     private NetworkStateReceiver networkStateReceiver;
     private GoogleMap mMap;
+    private Marker myLocation;
+    private Circle circle;
+
+    // GPS用
+    private LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gourmet_search);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(Objects.requireNonNull(this),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,}, LOCATION_REQUEST);
+            return;
+        }
+        setup();
+    }
+
+    private void setup() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         networkStateReceiver = new NetworkStateReceiver();
         networkStateReceiver.addListener(this);
         this.registerReceiver(networkStateReceiver, new IntentFilter(android.net.ConnectivityManager.CONNECTIVITY_ACTION));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                10, // 通知のための最小時間間隔（ミリ秒）
+                10, // 通知のための最小距離間隔（メートル）
+                new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        String msg = "Lat=" + location.getLatitude()
+                                + "\nLng=" + location.getLongitude();
+                        Log.d("GPS", msg);
+                        moveMap2Location(location, mMap.getCameraPosition().zoom);
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                    }
+                }
+        );
+
     }
 
     public void onDestroy() {
@@ -37,23 +108,77 @@ public class GourmetSearchActivity extends FragmentActivity implements OnMapRead
         networkStateReceiver.removeListener(this);
         this.unregisterReceiver(networkStateReceiver);
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        for (String provider : providers) {
+
+            Location l = mLocationManager.getLastKnownLocation
+                    (provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+            }
+        }
+        Log.d("last", "getLastKnownLocation " + bestLocation);
+        moveMap2Location(bestLocation, 15.0f);
+    }
+
+    private void moveMap2Location(Location location, float zoom) {
+        drawCircle(location);
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        if (myLocation != null) {
+            myLocation.remove();
+        }
+
+        Bitmap current_pin = BitmapUtil.resizeMapIcons(getApplicationContext(), "current_pin", 100, 100);
+        myLocation = mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.fromBitmap(current_pin))
+                .anchor(0.5f, 0.5f)
+        );
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
+        mMap.moveCamera(cameraUpdate);
+    }
+
+    private void drawCircle(Location location) {
+        if (circle != null) {
+            circle.remove();
+        }
+        circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(location.getLatitude(), location.getLongitude()))
+                .radius(500)
+                .strokeWidth(2)
+                .strokeColor(getColor(R.color.colorPrimary))
+                .fillColor(getColor(R.color.colorPrimaryTransparent)));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST) {
+            setup();
+        }
     }
 
     @Override
     public void networkAvailable() {
-        Log.d("GourmetSearchActivity","Connected");
+        Log.d("GourmetSearchActivity", "Connected");
     }
 
     @Override
     public void networkUnavailable() {
-        Log.d("GourmetSearchActivity","Disonnected");
+        Log.d("GourmetSearchActivity", "Disonnected");
     }
 }
